810288e84775fa3b18f39f8d38a4ab5256341609
Umair Ijaz <umairkhosa398@gmail.com>
2024-04-01 21:51:37 +0500

    added ng google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngmap';
+
+  zoom = 12;
+  center: google.maps.LatLngLiteral = { lat: 33.643178341078425, lng: 73.07935530906504 };
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  };
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
+  }
+
+  zoomIn() {
+    if (this.zoom < 100) { this.zoom++; }
+  }
+
+  zoomOut() {
+    if (this.zoom > 20) { this.zoom-- };
+  }
 }
